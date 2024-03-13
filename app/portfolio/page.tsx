77af2b95fc1632ea1028b6481237d3867d42f97c
quantumbuilder32 <quantumbuilder32@gmail.com>
2024-03-13 01:50:17 -0500

    cool

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -3,6 +3,14 @@ import styles from "./page.module.css"
 
 export default function Page() {
     return (
-        <div>page</div>
+        <main>
+            <section>
+                <div style={{ textAlign: "center", display: "grid", justifyItems: "center" }}>
+                    <p className='supportingTitle2'>portfolio</p>
+                    <h1>Our Projects</h1>
+                    <p>Coming Soon</p>
+                </div>
+            </section>
+        </main>
     )
 }
